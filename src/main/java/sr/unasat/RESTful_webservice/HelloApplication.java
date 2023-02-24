5b002bd18467928a903f5758f825395badf56485
Famy <f.semoedi@unasat.sr>
2023-02-24 18:07:52 -0300

    first commit

---
+++ src/main/java/sr/unasat/RESTful_webservice/HelloApplication.java
@@ -0,0 +1,9 @@
+package sr.unasat.RESTful_webservice;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
